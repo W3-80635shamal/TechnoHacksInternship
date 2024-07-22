130f7407d307768bea0eeeecfd834cdfee336b94
W3-80635 <shmlrmwt@gmail.com>
2024-07-22 23:45:06 +0530

    Task-2

---
+++ Task-2/portfolio/src/App.js
@@ -0,0 +1,26 @@
+// import React from 'react'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Header from './components/Header'
+// import Home from './components/Home';
+// import About from './components/About';
+// import Portfolio from './components/Portfolio';
+// import Experience from './components/Experience';
+// import Contact from './components/Contact';
+
+// function App() {
+//   return (
+//     <Router>
+//     <Header />
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/about" element={<About />} />
+//       <Route path="/portfolio" element={<Portfolio />} />
+//       <Route path="/experience" element={<Experience />} />
+//       <Route path="/contact" element={<Contact />} />
+//     </Routes>
+//   </Router>
+    
+//   );
+// }
+
+// export default App;
---
+++ Task-2/portfolio/src/components/Contact.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Contact() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Form data submitted:', formData);
+    navigate('/'); 
+  };
+
+  return (
+    <div className="max-w-screen-2xl container mx-auto px-4 md:px-20 mt-10">
+      <h1 className="text-center text-3xl font-bold  mb-5 my-20">Contact</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
